54640ed2f4f461e0bfd2c14b137fc6b5d0081594
hpabnerdom <abnerdomhp@hotmail.com>
2020-04-02 13:16:39 -0600

    Update Main.java

--- Tarea30marzo/src/Main.java
+++ Tarea30marzo/src/Main.java
@@ -11,9 +11,9 @@ public class Main {
         System.out.println("1a Calle A, Colonia Villas del Magisterio Zona 9");
         System.out.println("58385781");
         
-        System.out.println("Nombre: Luis Humberto Ruiz Castillo");
-        System.out.println("Direccion: 8va Calle A 11-30 Zona 1 interior ");
-        System.out.println("Telefono: 37070944");
+        System.out.println("Nombre: " + "Abner Obed Domínguez Rodríguez");
+        System.out.println("Dirección: " + "Colonia El Oasis, Zona 8, Huehuetenango");
+        System.out.println("Numero Tel: " + "42252923");
         
     }
     
