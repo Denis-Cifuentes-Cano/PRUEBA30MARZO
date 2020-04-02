98716430e57d732d0555cefc8e6b30a648ee2361
Carlos Andrés Miranda Méndez <62572969+CarlosMM28@users.noreply.github.com>
2020-04-02 12:54:39 -0600

    Update Main.java

--- Tarea30marzo/src/Main.java
+++ Tarea30marzo/src/Main.java
@@ -6,6 +6,10 @@ public class Main {
         System.out.println("Nombre: " + "Denis Joel Cifuentes Cano");
         System.out.println("Direccion: "+ "Zona 1 Chiantla");
         System.out.println("Numero cel: " + "45883960");
+        
+        System.out.println("Carlos Andrés Miranda Méndez");
+        System.out.println("1a Calle A, Colonia Villas del Magisterio Zona 9");
+        System.out.println("58385781");
     }
     
 }
